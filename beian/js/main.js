03ae9f45fa11753c70dc4fb0e7926d2528d652bf
zonghua2016 <2280314971@qq.com>
2016-09-27 18:06:57 +0800

    add zhongkebeian

---
+++ beian/js/main.js
@@ -0,0 +1,42 @@
+
+$(function(){
+//	登陆框弹出
+	var $whole=$('.whole');
+	$whole.animate({'right':0},1000);
+	
+	var $login=$('.login').eq(0);
+	var smPic=[];
+	//定时器 
+	var smailTimer=setInterval(function(){
+		//产生随机位置
+		var numLeft=Math.round(Math.random()*50);
+		var numTop=Math.round(Math.random()*200+200);
+		
+		//创建一个img元素
+		var $loginImg=$('<img/>');
+		$loginImg.attr({'src':'./images/hu.jpg'});
+		$loginImg.css({'left':numLeft+'%','top':numTop+'px'});
+		$login.append($loginImg);
+		smPic.push($loginImg);
+		
+		//不断循环删除新增加的img元素
+		for(var i=0;i<smPic.length;i++){
+			if(smPic[i].css('opacity')=='0'){
+				smPic[i].remove();
+			}
+		}
+	},1000); 
+	
+//	登陆效果
+	var $clearInput=$('.login_close_img');
+		
+		var $oInput=$('.input');
+		$clearInput.eq(0).click(function(){
+			$oInput.eq(0).val('');
+		})
+		$clearInput.hover(function(){
+			$(this).attr('src','images/login_close_hover.png');
+		},function(){
+			$(this).attr('src','images/login_close.png');
+		});
+});
