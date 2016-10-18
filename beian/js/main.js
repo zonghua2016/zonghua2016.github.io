a1a8164ad00caa64c4f63d7c33e254d32cbbfbe7
zonghua2016 <3338788099@qq.com>
2016-10-18 22:14:21 +0800

    all

--- beian/js/main.js
+++ beian/js/main.js
@@ -14,26 +14,40 @@ $(function(){
 		
 		//创建一个img元素
 		var $loginImg=$('<img/>');
-		$loginImg.attr({'src':'./images/hu.jpg'});
-		$loginImg.css({'left':numLeft+'%','top':numTop+'px'});
-		$login.append($loginImg);
+		$loginImg.attr({'src':'./images/smailpic.png'}).css({'left':numLeft+'%','top':numTop+'px'}).appendTo($login);
+//		$login.append($loginImg);
 		smPic.push($loginImg);
-		
+
+//	var loginImg=document.createElement('img');
+//		loginImg.style.left=numLeft+'%';
+//		loginImg.style.top=numTop+'px';
+//		loginImg.style.src='./images/smailpic.png';
+//		loginImg.style.opacity=1;
+//		$login.appendChild(loginImg);
+//		
+//		smPic.push(loginImg);
 		//不断循环删除新增加的img元素
 		for(var i=0;i<smPic.length;i++){
 			if(smPic[i].css('opacity')=='0'){
 				smPic[i].remove();
 			}
 		}
+				
 	},1000); 
 	
 //	登陆效果
 	var $clearInput=$('.login_close_img');
-		
 		var $oInput=$('.input');
 		$clearInput.eq(0).click(function(){
 			$oInput.eq(0).val('');
-		})
+		});
+		$clearInput.eq(1).click(function(){
+			$oInput.eq(1).val('');
+		});
+		
+//		$clearInput.click(function(){
+//			$oInput.eq($(this).index()).val('');
+//		});
 		$clearInput.hover(function(){
 			$(this).attr('src','images/login_close_hover.png');
 		},function(){
