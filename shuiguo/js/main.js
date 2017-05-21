cb2419a51df7caef6dde168f9462e7f0cda43a47
zonghua2016 <3338788099@qq.com>
2017-05-21 14:06:45 +0800

    Update main.js

--- shuiguo/js/main.js
+++ shuiguo/js/main.js
@@ -1,4 +1,17 @@
 $(function() {
+	if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+	   //alert(navigator.userAgent); 
+	   
+	   //add a new meta node of viewport in head node
+		head = document.getElementsByTagName('head');
+		viewport = document.createElement('meta');
+		viewport.name = 'viewport';
+		viewport.content = 'target-densitydpi=device-dpi, width=' + 750 + 'px, user-scalable=no';
+		head.length > 0 && head[head.length - 1].appendChild(viewport);    
+	   
+	}
+	//页面不足一屏，铺满一屏
+	$('.wrap').css('min-height',$(window).height());
 	var $tel = $('.mobileLogin .mobileNum input');
 	var $close = $('.mobileLogin .mobileNum .close');
 	var $proLi = $('.products .proGroup .pros li');
